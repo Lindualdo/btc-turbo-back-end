3aecdcdc0594a0fe536c77b08d5f2813768f396d
Lindualdo <lindualdo@hotmail.com>
2025-05-24 22:18:17 +0100

    v 1.0.20 - Fix referencias realized_price.utils

--- app/services/btc_analysis.py
+++ app/services/btc_analysis.py
@@ -7,7 +7,6 @@ import requests
 from app.config import get_settings
 import logging
 from app.utils.m2_utils import get_m2_global_momentum
-from app.utils.realized_price_utils import analyze_btc_vs_realized_price
 from typing import Tuple
 
 
